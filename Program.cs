5f9c52147323431ec12ff46c4a767facf3b78d72
1YusifBedel1 <tu7hqak2t@code.edu.az>
2024-04-02 09:47:05 +0400

    commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using ConsoleApp10;
+
+Weopan weopan = new Weopan(15, 10, false);
+
+while (true)
+{
+    Console.WriteLine("Seçiminiz (0 - İnformasiya, 1 - Ateş et, 2 - Qalan gülələrin sayı, 3 - Darağı yenidən doldur, 4 - Atış modunu dəyiş, 5 - Çıxış): ");
+    string action = Console.ReadLine();
+
+    switch (action)
+    {
+        case "0":
+            Console.WriteLine("C- Atış modunu dəyişir.");
+            break;
+        case "1":
+            weopan.Shoot();
+            break;
+        case "2":
+            Console.WriteLine("Qalan gülələrin sayı: " + weopan.GetRemainBulletCount());
+            break;
+        case "3":
+            weopan.Reload();
+            break;
+        case "4":
+            weopan.ChangeFireMode();
+            break;
+        case "5":
+            Console.WriteLine("Proqram dayandırılır.");
+            return;
+        default:
+            Console.WriteLine("Düzgün seçim edin.");
+            break;
+    }
+}
---
+++ Weopan.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Weopan
+    {
+        private bool fireMode;
+
+        public byte CapacityBullet { get; set; }
+        public byte CurrentBullet { get; set; }
+        public bool FireMode { get; set; }
+        public Weopan(byte capacityBullet, byte currentBullet, bool fireMode)
+        {
+            this.CapacityBullet = capacityBullet;
+            this.CurrentBullet = currentBullet;
+            this.FireMode = fireMode;
+        }
+        public void Shoot()
+        {
+
+            if (FireMode = true)
+            {
+                Console.WriteLine("avtomatik");
+            }
+            else
+            {
+                Console.WriteLine("tekli");
+            }
+
+
+        }
+        public int GetRemainBulletCount()
+        {
+           return CapacityBullet - CurrentBullet;
+            Console.WriteLine(11);
+
+        }
+        public void Reload()
+        {
+            CurrentBullet = CapacityBullet;
+            Console.WriteLine("daragi doldur");
+        }
+        public void ChangeFireMode()
+        {
+            fireMode = !fireMode;
+            if (fireMode)
+            {
+                Console.WriteLine("Atış modu: Avtomatik");
+            }
+            else
+            {
+                Console.WriteLine("Atış modu: Təkli");
+            }
+        }
+    }
+}
